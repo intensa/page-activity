c41b3d6c55890a0285db3b4150bbf09a848a28f2
IvanShishkin <i.shishkin@intensa.ru>
2020-11-15 15:53:26 +0300

    init

---
+++ app.js
@@ -0,0 +1,102 @@
+class PageActivity {
+
+
+    constructor(element, setting = {}) {
+
+        this.eventFlag = 0;
+        let defaultSettings = {
+            achieveTime: 60,
+            loop: 0,
+            eventList: 'touchmove blur focus focusin focusout load resize scroll unload click dblclick mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave change select submit keydown keypress keyup error',
+            testPeriod: 10,
+            useMultiMode: true,
+            callBack: function (e) {
+                console.log('Achieved!')
+            },
+            watchEvery: 1,
+            counter: {test: 0, achiev: 0}
+        }
+
+        this.settings = Object.assign(defaultSettings, setting);
+        this.settings.watchEvery *= 1000;
+
+        this.bindElement = element;
+
+        if (setting.useMultiMode) {
+            this.loadMultiData();
+        }
+
+
+        this.bindEvent();
+        this.process();
+        console.log(this.eventFlag)
+    }
+
+    bindEvent() {
+        this.settings.eventList.split(' ').map((event) => {
+            this.bindElement.addEventListener(event, this.eventTrigger.bind(this));
+        });
+
+    }
+
+    process() {
+        console.log(this.eventFlag );
+        this.settings.counter.test += 1;
+
+        if (this.settings.counter.test === this.settings.testPeriod) {
+            if (this.eventFlag) {
+                this.eventFlag = 0;
+                this.settings.counter.achiev += this.settings.testPeriod;
+            }
+            this.settings.counter.test = 0;
+        }
+
+        let timerHand = setTimeout(() => { this.process() }, this.settings.watchEvery);
+        //console.log(timerHand)
+
+        if (this.settings.counter.achiev >= this.settings.achieveTime) {
+            if (!this.settings.loop) clearTimeout(timerHand);
+            this.settings.counter.achiev = this.settings.loop ? 0 : -1;
+            this.settings.callBack.call(this);
+        }
+
+        if (this.settings.useMultiMode) document.cookie = 'activity=' + this.settings.counter.test + '|' + this.settings.counter.achiev + '; path=/;';
+
+
+    }
+
+    loadMultiData() {
+        let search = ' activity=';
+        let cookie = ' ' + document.cookie;
+
+        if (cookie.length > 0) {
+            if (cookie.indexOf(search) != -1) {
+                let offset = cookie.indexOf(search) + search.length;
+                var m = unescape(cookie.substring(offset, cookie.indexOf(";", offset) == -1 ? cookie.length : cookie.indexOf(";", offset))).split('|');
+                this.settings.test = parseInt(m[0]);
+                this.settings.achiev = parseInt(m[1]);
+                return;
+            }
+        }
+
+        this.settings.counter.test = this.settings.counter.achiev = 0;
+    }
+
+    eventTrigger() {
+        console.log('run event');
+        this.eventFlag = 1;
+        //console.log(this.eventFlag);
+    }
+}
+
+let el = document;
+let test = new PageActivity(el, {
+    loop: 0,
+    achieveTime: 5,
+    callBack: function () {
+        // тут счеткик
+        console.log('сработал счетчик1');
+    }
+});
+
+console.log(test);
