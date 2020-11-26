59e76a11859fea736a4c801cff7f0fdcf6ae9912
IvanShishkin <i.shishkin@intensa.ru>
2020-11-26 15:21:21 +0300

    dev

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: './index.js',
+        library: 'myApp'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                        ],
+                    },
+                },
+            }
+        ]
+    }
+};
